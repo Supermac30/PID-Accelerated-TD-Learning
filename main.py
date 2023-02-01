de7da6fe64bfa816f5e89518a7d48e59c5ddec3c
Mark Bedaywi <mark.bedaywi@mail.utoronto.ca>
2023-02-01 11:35:42 -0500

    Did some clean up and fixed a bug

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+"""
+As a dynamical system that we can apply control theory to,
+value iteration is of the form:
+V_{k + 1} = V_k + u_k
+where u_k is the output of the controller.
+
+To do implement this, we build various controllers using a Controller class,
+and allow for multiple controllers to be attached to the MDP.
+By building this, we can compose controllers together, taking the sum of their
+outputted u_k. This allows us to build a single P, I, and D controller class, and build
+all possible controllers from there, as well as easily test different controllers, or mix and match.
+"""
+
 import numpy as np
 
 class MDP:
     """
-    Represents a discounted MDP ({x_1, ..., x_n}, {a_1, ..., a_m}, R, P, gamma).
+    Represents a discounted MDP with states
+    {x_1, ..., x_{num_of_states}} and actions {a_1, ..., a_{num_of_actions}}.
+
+    self.R is a vector of rewards where R[i] = R(x_i)
+    self.P is the transition probability kernel of some policy
+    self.gamma is the discount factor
     """
     def __init__(self, num_of_states, num_of_actions, R, P, gamma):
         self.num_of_states = num_of_states
@@ -13,27 +31,12 @@ class MDP:
 
         self.controllers = []
 
-    def attach_controllers(self, *controllers):
-        """Attaches the inputted controllers to the MDP, so that their valuation
-        is added to the dynamical system during VI.
-        """
-        self.controllers.extend(controllers)
-
-    def bellman_operator(self, V):
-        """Computes T^pi(V), where T^pi is the Bellman operator of the policy pi
-        self.P is the transition probability kernel of."""
-        return self.R + self.gamma * self.P @ V
-
     def value_iteration(self, num_iterations=1000):
         """Compute the value function via VI using the added controllers.
-
-        The plant is a simple integrator with u_k as its input,
-        V_{k + 1} = V_k + u_k
-        where u_k is the sum of all attached controllers.
         """
         # V1 is the current value function, V0 is the previous value function
-        V0 = np.zeroes((self.num_of_states, 1))
-        V1 = np.zeroes((self.num_of_states, 1))
+        V0 = np.zeroes((1, self.num_of_states))
+        V1 = np.zeroes((1, self.num_of_states))
 
         for _ in range(num_iterations):
             TV = self.bellman_operator(V1)
@@ -41,6 +44,16 @@ class MDP:
 
         return V1
 
+    def bellman_operator(self, V):
+        """Computes T^pi(V), where T^pi is the Bellman operator of the policy pi
+        self.P is the transition probability kernel of."""
+        return self.R + self.gamma * self.P @ V
+
+    def attach_controllers(self, *controllers):
+        """Attaches the inputted controllers to the MDP, so that their valuation
+        is added to the dynamical system during VI.
+        """
+        self.controllers.extend(controllers)
 
 
 class Controller:
@@ -53,14 +66,8 @@ class Controller:
     A controller is a function that will output u_k given the error. We don't have access to the
     error, and so we have the controller be a function of T(V_k), V_k, V_{k - 1},
     which is all that is needed for the P, PI, PD, and PID controllers.
-
-    By building this, we can compose controllers together, by taking the sum of their
-    outputted u_k. This allows us to build a single P, I, and D controller class, and build
-    all possible controllers from there.
-
-    This allows us to easily test different types controllers and mix and match
     """
-    def evaluate_controller(self, TV_k, V_k, V_k_minus_1):
+    def evaluate_controller(self, TV, V, V_prev):
         raise NotImplementedError
 
 
@@ -68,8 +75,8 @@ class P_Controller(Controller):
     def __init__(self, Kp):
         self.Kp = Kp
 
-    def evaluate_controller(self, TV_k, V_k, V_k_minus_1):
-        BR = TV_k - V_k
+    def evaluate_controller(self, TV, V, V_prev):
+        BR = TV - V
         return self.Kp @ BR
 
 
@@ -80,9 +87,9 @@ class I_Controller(Controller):
         self.Ki = Ki
         self.z = initial_z
 
-    def evaluate_controller(self, TV_k, V_k, V_k_minus_1):
-        BR = TV_k - V_k
-        evaluation = self.Ki * (self.beta @ self.z + self.alpha @ BR)
+    def evaluate_controller(self, TV, V, V_prev):
+        BR = TV - V
+        evaluation = self.Ki @ (self.beta @ self.z + self.alpha @ BR)
         self.z = self.beta + self.alpha @ BR
 
         return evaluation
@@ -92,5 +99,5 @@ class D_Controller(Controller):
     def __init__(self, Kd):
         self.Kd = Kd
 
-    def evaluate_controller(self, TV_k, V_k, V_k_minus_1):
-        return self.Kd @ (V_k - V_k_minus_1)
+    def evaluate_controller(self, TV, V, V_prev):
+        return self.Kd @ (V - V_prev)
