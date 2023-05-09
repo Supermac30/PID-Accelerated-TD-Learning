0fab037c575747c62ef110490ac76bddff68acbe
Mark Bedaywi <50279151+Supermac30@users.noreply.github.com>
2023-05-09 12:03:08 -0400

    Fixed small bugs

--- Experiments/VIExperiments/VIPolicyEvaluation.py
+++ Experiments/VIExperiments/VIPolicyEvaluation.py
@@ -9,9 +9,9 @@ def policy_evaluation_experiment(cfg):
     """Attempt to replicate results in figure 1 of PID Accelerated VI"""
     for kp, kd, ki, alpha, beta in zip(cfg['kp'], cfg['kd'], cfg['ki'], cfg['alpha'], cfg['beta']):
         agent, env, policy = build_agent_and_env(("VI", kp, ki, kd, alpha, beta), cfg['env'], False, cfg['seed'], cfg['gamma'])
-        history, _ = agent.policy_evaluation(
+        history, _ = agent.value_iteration(
             num_iterations=cfg['num_iterations'],
-            test_function=build_test_function(cfg['norm'], find_Vpi(env, policy))
+            test_function=build_test_function(cfg['norm'], find_Vpi(env, policy, cfg['gamma']))
         )
         save_array(history, f"kp={kp} kd={kd} ki={ki} alpha={alpha} beta={beta}", plt)
 
