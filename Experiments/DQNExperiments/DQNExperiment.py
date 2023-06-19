dbc5e3ac0626a070acd27fab57445871fecf5d83
bedaywim <bedaywim@gpu026.cluster.local>
2023-06-19 10:59:57 -0400

    Added more hyperparameters

--- Experiments/DQNExperiments/DQNExperiment.py
+++ Experiments/DQNExperiments/DQNExperiment.py
@@ -14,20 +14,20 @@ def control_experiment(cfg):
     for kp, kd, ki, alpha, beta, optimizer, \
         replay_memory_size, batch_size, learning_rate, \
         tau, initial_eps, exploration_fraction, \
-        minimum_eps, epsilon_decay_step, train_step, target_update_interval, d_tau, \
-        inner_size, adapt_gains, epsilon, meta_lr in \
+        minimum_eps, gradient_steps, train_freq, target_update_interval, d_tau, \
+        inner_size, adapt_gains, epsilon, meta_lr, learning_starts in \
         zip(cfg['kp'], cfg['kd'], cfg['ki'], cfg['alpha'], cfg['beta'], cfg['optimizer'], \
             cfg['replay_memory_size'], cfg['batch_size'], cfg['learning_rate'], \
             cfg['tau'], cfg['initial_eps'], cfg['exploration_fraction'], \
-            cfg['minimum_eps'], cfg['epsilon_decay_step'], cfg['train_step'], cfg['target_update_interval'], \
-            cfg['d_tau'], cfg['inner_size'], cfg['adapt_gains'], cfg['epsilon'], cfg['meta_lr']):
+            cfg['minimum_eps'], cfg['gradient_steps'], cfg['train_freq'], cfg['target_update_interval'], \
+            cfg['d_tau'], cfg['inner_size'], cfg['adapt_gains'], cfg['epsilon'], cfg['meta_lr'], cfg['learning_starts']):
         agent = build_PID_DQN(
             kp, ki, kd, alpha, beta, 
             cfg['env'], cfg['gamma'], optimizer,
             replay_memory_size, batch_size, learning_rate,
             tau, initial_eps, exploration_fraction,
-            minimum_eps, epsilon_decay_step, train_step, target_update_interval,
-            d_tau, inner_size, cfg['slow_motion'],
+            minimum_eps, gradient_steps, train_freq, target_update_interval,
+            d_tau, inner_size, cfg['slow_motion'], learning_starts,
             tensorboard_log=cfg['tensorboard_log'], seed=seed,
             adapt_gains=adapt_gains, meta_lr=meta_lr, epsilon=epsilon
         )
--- TabularPID/AgentBuilders/DQNBuilder.py
+++ TabularPID/AgentBuilders/DQNBuilder.py
@@ -5,9 +5,9 @@ from TabularPID.Agents.DQN.DQN import PID_DQN
 import logging
 
 def build_PID_DQN(kp, ki, kd, alpha, beta, env_name, gamma, optimizer, replay_memory_size, batch_size,
-                  learning_rate, tau, initial_eps, exploration_fraction, minimum_eps, epsilon_decay_step,
-                  train_step, target_update_interval, d_tau, inner_size, slow_motion, tensorboard_log=None, seed=42,
-                  adapt_gains=False, meta_lr=0.1, epsilon=0.1):
+                  learning_rate, tau, initial_eps, exploration_fraction, minimum_eps,
+                  gradient_steps, train_freq, target_update_interval, d_tau, inner_size, slow_motion, learning_starts,
+                  tensorboard_log=None, seed=42, adapt_gains=False, meta_lr=0.1, epsilon=0.1):
     """Build the PID DQN agent
     """
     env = create_gym_wrapper(env_name, slow_motion=slow_motion)
@@ -19,15 +19,16 @@ def build_PID_DQN(kp, ki, kd, alpha, beta, env_name, gamma, optimizer, replay_me
         env=env,
         learning_rate=learning_rate,
         buffer_size=replay_memory_size,
-        learning_starts=batch_size, # Buffer size starts at batch size
         batch_size=batch_size,
         tau=tau,
         gamma=gamma,
-        gradient_steps=train_step,
+        gradient_steps=gradient_steps,
+        train_freq=train_freq,
         target_update_interval=target_update_interval,
         exploration_fraction=exploration_fraction,
         exploration_initial_eps=initial_eps,
         exploration_final_eps=minimum_eps,
+        learning_starts=learning_starts,
         tensorboard_log=tensorboard_log,
         policy_kwargs=dict(net_arch=[inner_size, inner_size],
                            optimizer_class=optimizer_class),
