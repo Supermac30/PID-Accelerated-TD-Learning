Fixed a fatal off by one

TabularPID/Agents/LinearQ.py
@@ -81,8 +81,8 @@ class LinearTDQ():
         self.reset(reset_environment)
 
         # The history of the gains
-        self.gain_history = [np.zeros(num_iterations // (num_iterations // 100) + 1) for _ in range(5)]
-        self.history = np.zeros(num_iterations // (num_iterations // 100) + 1)
+        self.gain_history = [np.zeros(num_iterations // (num_iterations // 100)) for _ in range(5)]
+        self.history = np.zeros(num_iterations // (num_iterations // 100))
         index = 0
 
         for k in range(num_iterations):
@@ -95,7 +95,6 @@ class LinearTDQ():
                 self.query_agent(next_state, next_action, component="Q")
                 for next_action in range(self.env.action_space.n)
             ])
-            breakpoint()
             current_state_Qp_value = self.query_agent(current_state, action, component="Qp")
             current_state_z_value = self.query_agent(current_state, action, component="z")
 
@@ -113,7 +112,6 @@ class LinearTDQ():
             self.w_z += lr_z * (z_update.item() - current_state_z_value.item()) * self.basis_value(current_state, action)
 
             if self.solved_agent is not None and k % (num_iterations // 100) == 0:
-                index += 1
                 self.history[index] = self.solved_agent.measure_performance(self.query_agent)
                 if adapt_gains:
                     self.update_gain_history(index)
@@ -121,6 +119,8 @@ class LinearTDQ():
                     # If we are too large, stop learning
                     self.history[index:] = float('inf')
                     break
+
+                index += 1
 
             if adapt_gains:
                 self.update_gains()